33bfbfa3beec3d905d44566884ba4355f30509b4
Hugo Gonzalez Labrador <github@hugo.labkode.com>
2020-11-30 10:13:36 +0100

    init

---
+++ deploy.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+set -e
+set -x
+
+echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
+
+rm -rf public
+git clone https://github.com/cs3community/cs32021 public
+hugo -t cs3
+cp CNAME public
+cd public
+
+# Commit changes.
+msg="rebuilding site `date`"
+if [ $# -eq 1 ]
+  then msg="$1"
+fi
+git add --all .
+git commit -m "$msg"
+
+# Push source and build repos.
+git push origin master
+
+# Come Back up to the Project Root
+cd ..
